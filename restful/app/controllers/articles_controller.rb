a6050d9d32d3c7511f8412bead56fbd96c0ec3c8
WinnyDrancourt <winny.morel@orange.fr>
2024-06-04 22:23:11 +0200

    fix

--- restful/app/controllers/articles_controller.rb
+++ restful/app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: %i[ index show ]
   before_action :set_article, only: %i[ show update destroy ]
-  before_action :authenticate_user!, only: %i[ create update destroy ]
+  before_action :authorize_owner, only: %i[ update destroy ]
 
   # GET /articles
   def index
@@ -16,7 +17,8 @@ class ArticlesController < ApplicationController
 
   # POST /articles
   def create
-    @article = Article.new(article_params)
+    # @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
 
     if @article.save
       render json: @article, status: :created, location: @article
@@ -48,5 +50,17 @@ class ArticlesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def article_params
       params.require(:article).permit(:title, :content)
+    end
+
+    def get_user_from_token
+      jwt_payload = JWT.decode(request.headers["Authorization"].split(" ")[1], Rails.application.credentials.devise[:jwt_secret_key]).first
+      user_id = jwt_payload["sub"]
+      User.find(user_id.to_s)
+    end
+
+    def authorize_owner
+      user = get_user_from_token
+      return render json: { error: "You are not authorized to perform this action." }, status: :unauthorized if user.nil?
+      render json: { error: "You are not authorized to perform this action." }, status: :unauthorized unless @article.user_id == user.id
     end
 end
